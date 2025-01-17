b8019a1a71860dda1610b6166fbd38c818a8eff2
DESKTOP-1DIAT6B\abh <ekenzo@naver.com>
2025-01-17 15:07:43 +0900

    250117

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,41 +1,57 @@
 require('dotenv').config();
 const pool = require('../config/db');
-
-const loginService = require('../services/loginService');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const accessTokenExpiry = '10m';  // 액세스 토큰 만료 시간
-const refreshTokenExpiry = '7d';  // 리프레시 토큰 만료 시간
 
+// 토큰생성
 const generateTokens = (user) => {
-  const accessToken = jwt.sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: accessTokenExpiry,
-  });
-
-  const refreshToken = jwt.sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: refreshTokenExpiry,
-  });
-
+  const accessToken = jwt.sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '10m'});
+  const refreshToken = jwt.sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'});
   return { accessToken, refreshToken };
 };
 
+// 비밀번호 검증하는 함수
+const comparePassword = async (plainPassword, hashedPassword) => {
+  try {
+    const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
+    return isMatch;
+  } catch (err) {
+    console.error("Error comparing passwords:", err);
+  }
+};
 
 exports.login = async (req, res) => {
   try {
-    // 유효성 검사 후, 사용자 정보 반환 (예시)
-    const { email, password } = req.body;   
-    const params = [email, password];
-    const user = await pool.query('SELECT * FROM tb_user WHERE user_id = $1 AND user_pw = $2', params)[0];
-    console.log(user);
-
-    if (user) {
+    const { email, password } = req.body;
+    // 비밀번호 해시
+    // const hashedPassword = await bcrypt.hash(password, 10);
+    const params = [email];
+    const user = await pool.query('SELECT * FROM tb_user WHERE user_id = $1', params);
+    const chk_pw = await comparePassword(password ,user.rows[0].user_pw);
+    if (chk_pw) {
       const { accessToken, refreshToken } = generateTokens(user);
     }
 
     // JWT 토큰 생성
-    const token = jwt.sign({ email: email, password: password }, SECRET_KEY, { expiresIn: '10s' });
+    const token = jwt.sign({ id: user.user_id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
     const result = {token: token};
     res.status(200).json(result);
+    
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+
+
+exports.join = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    
+    // 비밀번호 해시
+    // const hashedPassword = await bcrypt.hash(password, 10);
+    
+    res.status(200).json();
     
   } catch (error) {
     res.status(400).json(error.message);
